9813dcc8b50038da7677375ec1e23c0c2d69b793
Agustin Alba Chicar <ag.albachicar@gmail.com>
2020-11-18 21:40:30 -0300

    - Adds agent capacity to compute the path based on Dijkstra - Adds a function that depends on edge traffic. - Adds tick iteration of the agent.

--- src/platform_analytics/warehouse.py
+++ src/platform_analytics/warehouse.py
@@ -2,9 +2,11 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 class Warehouse:
-  def __init__(self, rows, cols, node_capacity):
+  def __init__(self, rows, cols, node_capacity, edge_base_cost=1., occupancy_cost=0.):
     self._rows = rows
     self._cols = cols
+    self._edge_base_cost = edge_base_cost
+    self._occupancy_cost = occupancy_cost
     self._graph = nx.Graph()
     # Add nodes
     for i in range(0, rows):
@@ -17,12 +19,18 @@ class Warehouse:
     # Add row edges
     for i in range(0, rows):
       for j in range(0, cols):
+        e = (Warehouse._get_node_name(i, j), Warehouse._get_node_name(i, j+1))
         self._graph.add_edge(Warehouse._get_node_name(i, j), Warehouse._get_node_name(i, j+1))
+        self._graph.edges[e]['weight'] = self._edge_base_cost
+        self._graph.edges[e]['occupancy'] = 0
   
     # Add column edges
     for i in range(0, rows-1):
       for j in range(0, cols+1):
+        e = (Warehouse._get_node_name(i, j), Warehouse._get_node_name(i+1, j))
         self._graph.add_edge(Warehouse._get_node_name(i, j), Warehouse._get_node_name(i+1, j))
+        self._graph.edges[e]['weight'] = self._edge_base_cost
+        self._graph.edges[e]['occupancy'] = 0
 
   def graph(self):
     return self._graph
@@ -35,6 +43,9 @@ class Warehouse:
     pos = {node_name: Warehouse._get_node_index(node_name) for node_name in self._graph.nodes}
     nx.draw(self._graph, pos=pos, with_labels=True, font_weight='bold')
     plt.show()
+
+  def get_edge_weight(self, n_i, n_j):
+    return self._graph.edges[(n_i, n_j)]['weight']
 
   def _get_node_name(row, col):
     return '{}_{}'.format(row, col)
